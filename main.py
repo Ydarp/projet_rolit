4716e6c2e80aa4d52e01629f7603a521dc164235
keriachan <djrais77@gmail.com>
2024-11-12 23:45:37 +0100

    rassemblages des fonction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from random import randint
 
-def tableau_depart():
-    t = [[None for i in range(8)] for i in range(8)]
-    t[3][3],t[3][4],t[4][3],t[4][4] = "r","j","b","v"
-    return t
-
 def nb_joueur():
     """
     Demande le nombre de joueur et le renvoie en entier entre 1 et 4
@@ -15,12 +10,12 @@ def nb_joueur():
 
 def pseudo(nb: int) :
     """
-    Demande le pseudo de chaque joueur, les renvoies dans une liste et renvoie le nombre de joueur
+    Demande le pseudo de chaque joueur et les renvoies dans une liste
     """
     joueur = []
-    for i in range (1,nb+1):
-        joueur.append(input("Quel est le pseudo du joueur numero "+ str(i) +" - "))
-    return nb,joueur
+    for i in range (nb):
+        joueur.append(input("Quel est le pseudo du joueur numero "+ str(i+1) +" - "))
+    return joueur
 
 def nb_manche():
     """
@@ -29,14 +24,14 @@ def nb_manche():
     nb_manche = input("Quel est le nombre de manche? - ")
     return nb_manche
 
-def couleur_joueur(joueur: list) -> list:
+def couleur_joueur(joueur: list, nb: int) -> list:
     """
     Attribue à chaque joueur une couleur et les renvoie dans une liste
     """
-    p, nb = joueur[1], joueur[0]
-    if nb == 2:
+    p, nb_j = joueur, nb
+    if nb_j == 2:
         p[0],p[1] = "rouge","vert"
-    elif nb == 3:
+    elif nb_j == 3:
         p[0],p[1],p[2] = "rouge","jaune","vert"
     else:
         p[0],p[1],p[2],p[4] = "rouge","jaune","vert","bleu"
@@ -62,11 +57,86 @@ def capture(t: list,x: int, y: int) -> None:
                 couleurs_capture.append(c)
     for c in couleurs_capture:
         t[c[0]][c[1]] = couleur
+    return t 
 
+def compte_couleur(t):
+    """Compte le nombre de pion de chaque couleur actuellement en jeu.
+
+    Args:
+        t (list): tableau a double entrée
+    Returns:
+        int : le nombre de rouge, jaune, bleu, vert
+    """
+    for i in range(8):
+        for j in range(8):
+            if t[i][j] == 'r':
+                r += 1
+            elif t[i][j] == 'j':
+                j += 1
+            elif t[i][j] == 'b':
+                b += 1
+            elif t[i][j] == 'V':
+                v += 1
+    return r, j, b, v
+
+def fin(t):
+    """Détermine si la manche est fini ou non.
+
+    Args:
+        t (list): tableau a double entrée
+
+    Returns:
+        bouléen_: Vrai si la manche est fini, faux sinon
+    """
+    for i in range(8):
+        for j in range(8):   
+            if t[i][j] == None:
+                return False  
+    return True   
+
+def couleur_jouee(t: list, x: int, y: int, c: str):
+    """Remplace t[x][y] par c
+
+    Args:
+        t (list): tableau a double entrée
+        x (int): coordonnée horizontale de la couleur
+        y (int): coordonnée verticale de la couleur
+        c (str): couleur 
+
+    Returns:
+        list: tableau avec la couleur jouée
+    """
+    t[y][x] = c
+    return t
+
+def affichage_tableau(t: list):
+    for i in range(8):
+        for j in range(8):
+            if j == 0 and t[i][j] != None:
+                print("|   ", t[i][j], " | ", end="")
+            elif j == 0 and t[i][j] == None:
+                print("| ", t[i][j], " | ", end="")
+            elif j != 0 and t[i][j] != None:
+                print("  ",t[i][j], " | ", end="")
+            else:
+                print(t[i][j], " | ", end="")
+        print()
+        
 def tableau_depart():
     t = [[None for i in range(8)] for i in range(8)]
     t[3][3],t[3][4],t[4][3],t[4][4] = "r","j","b","v"
     return t
-
-def main():
+   
+if __name__ == '__main__':
+    nb_j = nb_joueur()
+    p = pseudo(nb_j)
+    nb_m = nb_manche()
+    c_j = couleur_joueur(p, nb_j)
     t = tableau_depart()
+    c = 'r'
+    while fin(t) is False:
+        affichage_tableau(t)
+        x, y = int(input("Quel est la coordonnée horizontale de la couleur jouée - ")), int(input("Quel est la coordonnée verticale de la couleur jouée - "))
+        t = capture(couleur_jouee(t, x, y, c),x ,y)
+    print('c est fini')
+        
