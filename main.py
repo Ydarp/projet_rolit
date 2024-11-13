2f17ee05a65d5c30c0bc3ab0ea6a28c0fc5c9023
= <=>
2024-11-13 20:46:39 +0100

    Correction d'erreur + score

--- main.py
+++ main.py
@@ -21,20 +21,16 @@ def nb_manche():
     """
     Demande le nombre de manche et le renvoie en entier
     """
-    nb_manche = input("Quel est le nombre de manche? - ")
+    nb_manche = int(input("Quel est le nombre de manche? - "))
     return nb_manche
 
 def couleur_joueur(joueur: list) -> dict:
     """
-    Attribue à chaque joueur une couleur et les renvoie dictionnaire
+    Attribue à chaque couleur le pseudo d'un joueur et les renvoie dictionnaire
     """
-    d, nb_j = {}, len(joueur)
-    if nb_j == 2:
-        d[joueur[0]],d[joueur[1]] = "R","V"
-    elif nb_j == 3:
-        d[joueur[0]],d[joueur[1]],d[joueur[2]] = "R","J","V"
-    else:
-        d[joueur[0]],d[joueur[1]],d[joueur[2]],d[joueur[4]] = "R","J","V","B"
+    couleurs, d = ["R", "J", "V", "B"], {}
+    for i in range(len(joueur)):
+        d[couleurs[i]] = joueur[i]
     return d
 
 def capture(t: list,x: int, y: int) -> list:
@@ -52,12 +48,11 @@ def capture(t: list,x: int, y: int) -> list:
             xs += d[0]
             ys += d[1]
             couleur_temp.append((xs,ys))
-        if t[xs][ys] == couleur:
+        if t[xs + d[0]][ys + d[1]] == couleur:
             for c in couleur_temp:
                 couleurs_capture.append(c)
     for c in couleurs_capture:
         t[c[0]][c[1]] = couleur
-    return t 
 
 def compte_couleur(t):
     """Compte le nombre de pion de chaque couleur actuellement en jeu.
@@ -65,35 +60,14 @@ def compte_couleur(t):
     Args:
         t (list): tableau a double entrée
     Returns:
-        int : le nombre de rouge, jaune, bleu, vert
+        compteur (dict): nombre de fois que chaque couleur apparait dans t
     """
-    r, j, b, v = 0, 0, 0, 0
-    for i in range(8):
-        for k in range(8):
-            if t[i][j] == 'R':
-                r += 1
-            elif t[i][j] == 'J':
-                j += 1
-            elif t[i][j] == 'B':
-                b += 1
-            elif t[i][j] == 'V':
-                v += 1
-    return r, j, b, v
-
-def fin(t):
-    """Détermine si la manche est fini ou non.
-
-    Args:
-        t (list): tableau a double entrée
-
-    Returns:
-        bouléen_: Vrai si la manche est fini, faux sinon
-    """
-    for i in range(8):
-        for j in range(8):   
-            if t[i][j] == None:
-                return False  
-    return True   
+    compteur  = {'R': 0, 'J': 0, 'V': 0,'B': 0}
+    for e in t:
+        for c in e:
+            if c in compteur:
+                compteur[c] += 1
+    return compteur
 
 def couleur_jouee(t: list, x: int, y: int, c: str):
     """Remplace t[x][y] par c
@@ -107,45 +81,100 @@ def couleur_jouee(t: list, x: int, y: int, c: str):
     Returns:
         list: tableau avec la couleur jouée
     """
-    t[y][x] = c
+    t[x][y] = c
     return t
 
 def affichage_tableau(t: list):
+    """affiche un tableau lisible
+
+    Args:
+        t (list): tableau a double entrée
+    """
     for i in range(8):
-        for j in range(8):
-            if j == 0 and t[i][j] != None:
-                print("|   ", t[i][j], " | ", end="")
-            elif j == 0 and t[i][j] == None:
-                print("| ", t[i][j], " | ", end="")
-            elif j != 0 and t[i][j] != None:
-                print("  ",t[i][j], " | ", end="")
+        if i == 0:
+            print("|       |    0  |    1  |    2  |    3  |    4  |    5  |    6  |    7  |")
+        for j in range(9):
+            if j == 0:
+                print("|   ", i," | ", end="")
+            elif t[i][j-1] is not None:
+                print("  ", t[i][j-1], " | ", end="")
             else:
-                print(t[i][j], " | ", end="")
+                print(t[i][j-1], " | ", end="")
         print()
         
 def tourne_tableau(t: list):
-    temp = t[0]
-    t.pop(0)
-    t.append(temp)
+    """met le premier indice du tableau a la fin
+
+    Args:
+        t (list): tableau a double entree
+
+    Returns:
+        t (list): tableau a double entree
+    """
+    t.append(t.pop(0))
     return t
+
+def gagnant(d: dict):
+    """Détermine qu'elle cle du dictionnaire a la plus grande valeure
+
+    Args:
+        d (dict): dictionnaire
+
+    Returns:
+        tuple: (c: str,v: int)
+    """
+    v, c = 0, ""
+    for cle, val in d.items():
+        if val > v:
+            v, c = val, cle
+    return c, v  
+ 
+def additionne_dict(d1: dict,d2: dict):
+    """Additionne les valeures de 2 dictionnaires avec les mêmes clés, avec les clés correspondantent. 
+
+    Args:
+        d1 (dict)
+        d2 (dict)
+
+    Returns:
+        r (dict): somme des valeurs des deux dictionnaires avec les clés correspondantent.
+    """
+    r = {}
+    for cle in d1:
+        r[cle] = d1[cle] + d2[cle]
+    return r
+    
 def tableau_depart():
+    """tableau de départ
+
+    Returns:
+        t (list): tableau a double entrée
+    """
     t = [[None for i in range(8)] for i in range(8)]
     t[3][3],t[3][4],t[4][3],t[4][4] = "R","J","B","V"
     return t
-   
+
 if __name__ == '__main__':
-    nb_j = nb_joueur()
+    k = 0 #compteur de manche
+    d_g = {} #dictionnaire du score de la partie
+    nb_j,nb_m = nb_joueur(),nb_manche()
     p = pseudo(nb_j)
-    nb_m = nb_manche()
     c_j = couleur_joueur(p)
-    t_c = []
-    for cle, valeur in c_j.items():
-        t_c.append(valeur)
+    t_c = list(c_j.keys())
     t = tableau_depart()
-    while fin(t) is False:
-        c = t_c[0]
-        affichage_tableau(t)
-        x, y = int(input("Quel est la coordonnée horizontale de la couleur jouée - ")), int(input("Quel est la coordonnée verticale de la couleur jouée - "))
-        t = capture(couleur_jouee(t, x, y, c),x ,y)
-        t_c = tourne_tableau(t_c)
-    print(pseudo[0]," a gagné !")
+    while k < nb_m:
+        print("Manche numéro", k+1, "sur", nb_m)
+        while sum(compte_couleur(t).values()) != 64:
+            c = t_c[0]
+            affichage_tableau(t)
+            print("Tour de ", c_j[c],"qui est ",c,".")
+            x, y = int(input("Quel est la coordonnée verticale de la couleur jouée - ")), int(input("Quel est la coordonnée horizontale de la couleur jouée - "))
+            capture(couleur_jouee(t, x, y, c),x ,y)
+            t_c = tourne_tableau(t_c)
+        g = gagnant(compte_couleur(t))
+        d_g = compte_couleur(t) if k == 0 else additionne_dict(d_g, compte_couleur(t))
+        print(c_j[g[0]],"a gagné la manche avec un score de", g[1])
+        k += 1
+    g = gagnant(d_g)
+    print(c_j[g[0]],"a gagné la partie avec un score de", g[1])
+    
